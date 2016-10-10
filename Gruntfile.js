e068235e45463ebaeba5ff433f3d442871ac3795
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-10 20:26:05 +0530

    Initial commit
    SEED®: Build + Test System — d3-Olympics®.

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+
+/*!
+ * d3-Olympics®
+ * ____________________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 10-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Compass
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build + Test System/-- of "d3-Olympics®".
+ * ____________________________________________________________________
+ */
