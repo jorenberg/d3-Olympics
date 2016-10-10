365137a3a0ead9ee41f464f94023d381aa1f55a2
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-11 00:29:21 +0530

    Initial commit
    The Non-AMD JavaScript - for defining "d3-Olympics Plot".

---
+++ source/core/script/engine/d3.olympics.js
@@ -0,0 +1,16 @@
+/*!
+ * d3-Olympics®
+ * _______________________________________________________________
+ * The Non-AMD JavaScript - for defining "d3-Olympics Plot".
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 10-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _______________________________________________________________
+ */
