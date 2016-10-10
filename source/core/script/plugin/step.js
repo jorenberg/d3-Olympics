50ab79e11156cd2a48d905a9517c240f255602e9
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-11 00:08:54 +0530

    Initial commit
    step: An AMD loader plugin for loaders like RequireJS that will load scripts in steps.
    This allows us to load traditional scripts that do not use AMD in a sequential fashion.
    Help: https://github.com/requirejs/step

---
+++ source/core/script/plugin/step.js
@@ -0,0 +1,14 @@
+/*!
+ * ~~~~~
+ * step®      : An AMD loader plugin for loading scripts in steps.
+ * __________________________________________________________________
+ * @copyright : 2010 - 2014, The Dojo Foundation All Rights Reserved.
+ * @help      : http://github.com/requirejs/text
+ * @require   : requirejs
+ * @license   : MIT or new BSD license.
+ * __________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________
+ */
