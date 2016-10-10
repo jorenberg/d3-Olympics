0750aaa6dfde44af92a5740c108f40ef8218219c
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-11 00:16:56 +0530

    Initial commit
    d3-hive: A plugin for rendering Hive Plots.
    Help: https://github.com/d3/d3-plugins

---
+++ source/core/script/plugin/d3.hive.js
@@ -0,0 +1,14 @@
+/*!
+ * ~~~~~~~~
+ * d3-hive®   : A plugin for rendering Hive Plots.
+ * ____________________________________________________________
+ * @copyright : 2010 - 2015, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-plugins
+ * @license   : MIT
+ * @require   : requirejs
+ * ____________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________
+ */
