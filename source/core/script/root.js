2e511ab63285a2c862917e2488e8512eff0f12a4
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-10 23:01:46 +0530

    Initial commit
    The Non-AMD JavaScript — for defining "root" configurations.

---
+++ source/core/script/root.js
@@ -0,0 +1,19 @@
+/*!
+ * d3-Olympics®
+ * _______________________________________________________________
+ * The Non-AMD JavaScript — for defining "root" configurations.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 10-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "d3-Olympics®".
+ * _______________________________________________________________
+ */
