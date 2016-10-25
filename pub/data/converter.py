59b97258a1bd7e37a1a9edfdffaa782e60ea297e
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-26 01:06:01 +0530

    Initial commit
    The CSV -> JSON and JSON -> CSV Converter of d3-Olympics®.

---
+++ pub/data/converter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  ,-.
+#  ,     ,-.   ,-.
+# / \   (   )-(   )
+# \ |  ,.>-( * )-<
+#  \|,' (   )-(   )
+#   Y ___`-'   `-'
+#   |/__/   `-'
+#   |
+#   |
+#   |  -~PRABHAT~-
+# __|_____________
+
+# The CSV -> JSON and JSON -> CSV Converter of d3-Olympics.
+
+__authors__       = [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
+__license__       = 'Apache License'
+__date__          = '26-10-2016'
+__version__       = '1.0.0'
