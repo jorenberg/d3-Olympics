566d45c4c2bc1b11e24e65832aeef49527d5a67c
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-26 01:09:39 +0530

    Initial commit
    Utility for d3-Olympics® — To read/write files in to the directory.

---
+++ pub/data/utility.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  ,-.
+#  ,     ,-.   ,-.
+# / \   (   )-(   )
+# \ |  ,.>-( * )-<
+#  \|,' (   )-(   )
+#   Y ___`-'   `-'
+#   |/__/   `-'
+#   |
+#   |
+#   |  -~PRABHAT~-
+# __|_____________
+
+# Utility for d3-Olympics® — To read/write files in to the directory.
+
+__authors__       = [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
+__license__       = 'Apache License'
+__date__          = '26-10-2016'
+__version__       = '1.0.0'
