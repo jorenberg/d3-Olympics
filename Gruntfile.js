377c1797375f44abe32311d2b2072b1ec1d10d82
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-26 16:45:31 +0530

    Initial commit
    Defined Task(htmlhint) for grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,3 +92,17 @@ module.exports = function(grunt) {
     // 8. Development dependencies updates                                        |
     // 9. Release(s) with version updates                                         |
     // ---------------------------------------------------------------------------|
+    
+    // 1. Assembly --> 1.1 HTML Hint
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    htmlhint: {
+      options: {
+        htmlhintrc: './config/rules/.htmlhintrc'
+      },
+      html1: {
+        src: ['./app/*.html']
+      },
+      html2: {
+        src: ['./app/en/**/*.html']
+      }
+    },
